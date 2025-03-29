4b62e3497729b60d3bb02408bd5204b55dc9f715
mishra-sweta <swetamishra.liluah@gmail.com>
2025-03-29 21:44:09 +0530

    Basic React components

--- App.js
+++ App.js
@@ -1,25 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1", key: 1 }, [
-    React.createElement("h1", { key: 1 }, "I'm a h1 Sweta Mishra"),
-    React.createElement("h2", { key: 2 }, "I'm a h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2", key: 2 }, [
-    React.createElement("h1", { key: 1 }, "I'm a h1 tag"),
-    React.createElement("h2", { key: 2 }, "I'm a h2 tag"),
-  ]),
-]);
+const Title = () => {
+  return <h1>Hello, world in Title!</h1>;
+};
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React!"
-// );
-
-// console.log(heading); //returns a JS object and not a tag which is then replaced by root.render() into html tags
+const Heading = () => (
+  <div id="container">
+    <Title />
+    {Title()}
+    <h1>Heading Component!</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<Heading />);
